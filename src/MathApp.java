8f168db11bcdac97c15f610940294e7e84633ef8
dyaksa <diasnour0395@gmail.com>
2024-04-17 21:45:03 +0700

    math

---
+++ src/MathApp.java
@@ -0,0 +1,11 @@
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(100,20);
+        System.out.println(min);
+
+        var max = Math.max(100,20);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+    }
+}
